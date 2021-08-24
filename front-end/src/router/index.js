b174d389b8376c52bc9a7086b38eaff49488bfe1
MVour <m.vourtzoumis@gmail.com>
2021-08-24 12:32:42 +0300

    fixed router

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,20 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from '../components/HomePage.vue'
+import wlcm from '../components/wlcm.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
     {
+        path: "/",
+        name: "wlcm",
+        component: wlcm
+    },{
         path: "/HomePage",
         name: "HomePage",
-        component: HomePage
+        component: HomePage,
+        // children: [
+        //     {
+        //       path: "aggelies",
+        //       name: "aggelies",
+        //       component: AggeliesComp,
+        //     },
+        // ]
     },
 ];
 
 const router = new VueRouter({
     mode: "history",
-    base: '/',routes,
+    base: "/",routes,
 });
 
 export default router;

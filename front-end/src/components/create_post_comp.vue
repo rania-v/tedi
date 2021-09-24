451af56ad47a4f7a0f276438eaee73f7aaca4965
rania-v <rania-98@outlook.com>
2021-09-24 22:13:59 +0300

    try to connect post creation

--- front-end/src/components/create_post_comp.vue
+++ front-end/src/components/create_post_comp.vue
@@ -3,7 +3,7 @@
         <v-card class="teal lighten-4" elevation="0">
             <v-subheader class="d-flex justify-center">CREATE AN AWESOME POST</v-subheader>
             <v-card-text :class="color">
-                <v-textarea placeholder="Share your thoughts with your network" v-model="post_txt" v-on:click="focus_rows=3, create=true" v-on:blur="check_post" :rows="focus_rows" color="teal"></v-textarea>
+                <v-textarea placeholder="Share your thoughts with your network" v-model="new_post.content" v-on:click="focus_rows=3, create=true" v-on:blur="check_post" :rows="focus_rows" color="teal"></v-textarea>
             </v-card-text>
             <v-card-actions>
                 <v-row class="d-flex align-center pa-0">
@@ -12,7 +12,7 @@
                     <v-file-input class="mt-6" dense outlined color="teal" placeholder="attach file"></v-file-input>
                     <v-spacer></v-spacer>
                     <!-- <v-div> -->
-                        <v-btn v-if="create" :disabled="post_txt==null || post_txt==''" class="deep-purple white--text" v-on:click="post">post</v-btn>
+                        <v-btn v-if="create" :disabled="new_post.content==null || new_post.content==''" class="deep-purple white--text" v-on:click=" skata()/*this.createPost(this.new_post)*/">post</v-btn>
                         <v-spacer v-if="create"></v-spacer>
                     <!-- </v-div> -->
                 </v-row>
@@ -22,6 +22,8 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
     name: 'PostCreate',
     data() {
@@ -29,14 +31,17 @@ export default {
             color: 'white',
             focus_rows: '1',
             create: false,
-            post_txt: null,
+            new_post: {
+                content: ''
+            }
 
         }
     },
     methods: {
+        ...mapActions(['createPost']),
         check_post() {
             this.focus_rows=1;
-            if(this.post_txt == null || this.post_txt == "")
+            if(this.new_post.content == null || this.new_post.content == "")
             {    this.create = false;
                 return false;
             }
@@ -45,7 +50,10 @@ export default {
                 return true;
         },
         post() {
-            this.$emit('new_post', this.post_txt)
+            this.$emit('new_post', this.new_post.content)
+        },
+        skata(){
+            alert('cont: ', this.new_post.content);
         }
     }
 }

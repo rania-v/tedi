4844d3849a4ce4e598d3cb3e4ec4c543b666f570
rania-v <rania-98@outlook.com>
2021-09-25 18:02:33 +0300

    update skillset-personal info

--- front-end/src/components/personal_info.vue
+++ front-end/src/components/personal_info.vue
@@ -109,16 +109,16 @@
                                 <v-autocomplete clearable shaped filled solo type item-color="pink" color="pink"  rounded :items="allSkills" item-text="category" item-value="skill_list" v-model="catlist" label="Hard Skills Categories">
                                 </v-autocomplete>
                                 <v-card flat>
-                    <v-chip :class="{'ma-2': checkList(skill_list.list,skill)!=1, 'pink pink--text small ma-2':checkList(skill_list.list,skill)==1}"
-                    v-for="skill in catlist" :key="skill" outlined large v-on:click="ClickSkill(skill_list.list,skill)">
-                        {{skill}}
-                    </v-chip>
-                </v-card>
-                <!-- <v-card-actions>
-                                <v-spacer></v-spacer>
+                                    <v-chip :class="{'ma-2': checkList(skill_list.list,skill)!=1, 'pink pink--text small ma-2':checkList(skill_list.list,skill)==1}"
+                                    v-for="skill in catlist" :key="skill" outlined large v-on:click="ClickSkill(skill_list.list,skill)">
+                                        {{skill}}
+                                    </v-chip>
+                                </v-card>
+                                <!-- <v-card-actions>
+                                    <v-spacer></v-spacer>
                                     <v-btn v-if="!isEmpty(skill_list.list)" small text class="pink--text" v-on:click="skill_list.list = EmptyList()">clear</v-btn>
                                 </v-card-actions> -->
-                    <v-chip class="ma-1" v-for="skill in skill_list.list" :key="skill" color="deep-purple lighten-4" close @click:close="ClickSkill(skill_list.list,skill)">{{skill}}</v-chip>
+                                    <v-chip class="ma-1" v-for="skill in skill_list.list" :key="skill" color="deep-purple lighten-4" close @click:close="ClickSkill(skill_list.list,skill)">{{skill}}</v-chip>
                             </v-card>
                         </v-card-text>
                     </v-card>
